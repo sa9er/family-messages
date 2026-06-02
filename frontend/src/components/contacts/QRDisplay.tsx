05e6c009d9999f3188e729fdd56f21166a7d7e9e
PocoDev <developer@poco.internal>
2026-06-02 20:16:19 +0200

    Fix TypeScript config and replace QR library with API

--- frontend/src/components/contacts/QRDisplay.tsx
+++ frontend/src/components/contacts/QRDisplay.tsx
@@ -1,16 +1,17 @@
+import { useDeviceId } from '../../hooks/useDeviceId';
 
-import QRCode from 'qrcode.react';
-
-interface Props {
-  deviceId: string;
-}
-
-export const QRDisplay: React.FC<Props> = ({ deviceId }) => {
+export const QRDisplay = () => {
+  const deviceId = useDeviceId();
+  
+  // Generate a simple data URL QR code using an external API (works without extra dependencies)
+  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(deviceId)}`;
+  
   return (
     <div style={{ textAlign: 'center', padding: 20 }}>
       <h3>Your QR Code</h3>
-      <QRCode value={deviceId} size={150} />
-      <p>Share this QR code with friends to connect</p>
+      <img src={qrUrl} alt="QR Code" width={150} height={150} />
+      <p style={{ fontSize: 12, marginTop: 10 }}>Your Device ID: <code>{deviceId}</code></p>
+      <p>Share this QR code or ID with friends to connect</p>
     </div>
   );
 };
